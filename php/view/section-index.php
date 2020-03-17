7bb826a5fa8d73082cde4a40befa7e3d23be19d1
Ismail Aydin <i.aydin20014@gmail.com>
2020-03-17 23:59:24 +0100

    login form and BG photo

---
+++ php/view/section-index.php
@@ -0,0 +1,16 @@
+
+    <div class="login-form">
+        <h1 name="titleSite">WELCOME TO MY SCHOOL</h1>
+        <form class="form" action="" method="POST">
+            <select class="user" name="user" id="user">
+                <option value="admin">Admin</option>
+                <option value="teacher">Teacher</option>
+                <option value="student">Student</option>
+            </select><br>
+                <p>Email</p>
+                <input type="mail" name="loginMail" id="loginMail" required placeholder="Enter your email..."><br>
+                <p>Password</p>
+                <input type="password" name="password" id="password" required placeholder="Enter your password..."><br>
+                <input type="submit" name="" value="Sign in">
+        </form>
+    </div>
